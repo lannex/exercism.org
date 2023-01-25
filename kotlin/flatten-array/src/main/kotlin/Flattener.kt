4f89f573a47e1b311661702745065ce90cccc587
Shin, SJ <lannex.shin@gmail.com>
2023-01-25 13:23:37 +0900

    feat(kotlin): flatten-array

---
+++ kotlin/flatten-array/src/main/kotlin/Flattener.kt
@@ -0,0 +1,12 @@
+object Flattener {
+    fun flatten(source: Collection<Any?>): List<Any?> {
+        return source
+            .flatMap {
+                when (it) {
+                    is Collection<*> -> flatten(it)
+                    null -> emptyList()
+                    else -> listOf(it)
+                }
+            }
+    }
+}
