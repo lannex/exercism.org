985f0e307b1f6fa101eaec26162cae7a3f2a9ddb
Shin, SJ <lannex.shin@gmail.com>
2023-03-01 23:52:20 +0900

    chore: update delete script

--- delete-files.sh
+++ delete-files.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-rm -rf rust/**/target
+rm -rf kotlin/**/build kotlin/**/.gradle
+
+rm -rf rust/**/target rust/**/Cargo.lock
+
+rm -rf .pytest_cache python/**/__pycache__
 
 rm -rf typescript/**/node_modules typescript/**/yarn.lock
